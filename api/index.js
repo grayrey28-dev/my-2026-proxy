e7dff1addaf08d62d8a8237f6acee6b1c56b2e88
grayrey28-dev <grayrey28@gmail.com>
2026-04-01 13:35:26 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (req, res) => {
   // Replace the URL below with the site you want to unblock
-  const target = "https://www.netflix.com"; 
+  const target = "https://www.bing.com"; 
 
   const proxy = createProxyMiddleware({
     target,
